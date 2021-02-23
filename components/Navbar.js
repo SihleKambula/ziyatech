59789a251cd662f8b28d10dabdd2ef05f4252c17
Sihle Kambula <kambulasihle@gamil.com>
2021-02-23 13:52:22 +0200

    finished with the navbar with responsiveness included

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { useState } from "react";
+import style from "../styles/components/Navbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import cx from "classnames";
+// Component
+const Navbar = () => {
+  // controlling the navbar
+  const [show, setShow] = useState(false);
+  function handleMenu() {
+    setShow(!show);
+  }
+  return (
+    <nav className={style.nav}>
+      <div className={style.logo}>
+        <img src='assets/Ziyatech-sm.png' alt='ziyatech_logo' />
+      </div>
+      <div className={style.menu_btn} onClick={handleMenu}>
+        <FontAwesomeIcon icon={show ? faTimes : faBars} />
+      </div>
+      <ul
+        className={cx(
+          style.nav_links_container,
+          show ? style.show : style.hide
+        )}
+      >
+        <li className={style.nav_links}>
+          <Link href='/'>home</Link>
+        </li>
+        <li className={style.nav_links}>
+          <Link href='/'>services</Link>
+        </li>
+        <li className={style.nav_links}>
+          <button>
+            <Link href='/'>contact</Link>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import Navbar from "../components/Navbar";
+import Head from "next/head";
+
+//Component
+
 const IndexPage = () => {
   return (
     <>
+      <Head>
+        <title>Ziyatech</title>
+        <link
+          rel='shortcut icon'
+          href='assets/Ziyatech-sm.png'
+          type='image/x-icon'
+        />
+      </Head>
       <header>
-        <nav>
-          <p>Navbar</p>
-        </nav>
+        <Navbar />
       </header>
     </>
   );
