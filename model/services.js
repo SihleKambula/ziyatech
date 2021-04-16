76509606d0393c8268fe51c1d6d662758cc11778
SihleKambula <kambulasihle@gmail.com>
2021-04-16 22:13:45 +0200

    display adjustments

--- model/services.js
+++ model/services.js
@@ -11,15 +11,15 @@ const services = [
     heading: "Business Support",
     paragraph:
       "SME Support. Digitalization(Website creation, email configuration, e.commerce, virtual stores, Search Engine Optimization and copyrights) ",
-    btnText: "Purchase Options",
-    urlLink: "#",
+    btnText: "Click Here To Begin",
+    urlLink: "https://form.jotform.com/202595787914571",
   },
   {
     img: "assets/img3.png",
     heading: "Internet Services",
     paragraph: "Sale of data bundles for the major mobile service providers.",
     btnText: "Purchase Options",
-    urlLink: "",
+    urlLink: "#",
   },
 ];
 
