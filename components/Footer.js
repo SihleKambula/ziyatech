ba9bbf52061aa761eab340702d0163ac3d0dbaad
SihleKambula <kambulasihle@gmail.com>
2021-04-16 17:52:05 +0200

    added footer information

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,53 @@
 import style from "../styles/components/Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
+      <div className={style.business_details}>
+        <div className='address'>
+          <h3>Business Address</h3>
+          <p>
+            673 Emhlangeni street
+            <br /> Central Western Jabavu <br /> Joahnnesburg
+            <br /> 1809 <br />
+            On google maps:
+            <a
+              href='https://www.google.com/maps/place/ZiyaTech+Thabiso/@-26.2418871,27.8739164,15z/data=!4m5!3m4!1s0x0:0x2be7a9d6d7761ea5!8m2!3d-26.2418871!4d27.8739164'
+              target='blank'
+            >
+              click here
+            </a>
+          </p>
+        </div>
+        <div className={style.social_media}>
+          <h3>Social Media</h3>
+          <div>
+            <a href='https://wa.me/message/WIQN4VY6HP3OJ1' target='blank'>
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+            <p> +27833631270</p>
+          </div>
+          <div>
+            <a href='https://m.facebook.com/www.ziyatech.co.za/' target='blank'>
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <p>ZiyaTech</p>
+          </div>
+        </div>
+        <div className='business_infor'>
+          <h3>Business Information</h3>
+          <p>
+            Registration Number : 2021/548751/07
+            <br /> Enterprise Name : ZiyaTech (Pty) LTD
+            <br /> All forms are prescribed by the Minister of Trade and
+            <br />
+            Industry in terms of section 223 of the Companies Act,
+            <br /> 2008 (Act No. 71 of 2008).
+          </p>
+        </div>
+      </div>
       <p>©️Ziyatech 2021</p>
     </footer>
   );
